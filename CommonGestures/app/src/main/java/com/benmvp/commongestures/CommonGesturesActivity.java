517dbd83e741337cddc34b1e55732cf15cecac91
Ben Ilegbodu <ben.ilegbodu@eventbrite.com>
2015-08-21 08:52:04 -0700

    Detecting common gestures via http://www.techotopia.com/index.php/Detecting_Common_Gestures_using_Android_Studio_and_the_Gesture_Detector_Class

---
+++ CommonGestures/app/src/main/java/com/benmvp/commongestures/CommonGesturesActivity.java
@@ -0,0 +1,107 @@
+package com.benmvp.commongestures;
+
+import android.support.v4.view.GestureDetectorCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.widget.TextView;
+
+public class CommonGesturesActivity extends AppCompatActivity
+    implements GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener {
+
+    private TextView gestureText;
+    private GestureDetectorCompat gDetector;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_common_gestures);
+        gestureText = (TextView) findViewById(R.id.gestureStatusText);
+        gDetector = new GestureDetectorCompat(this, this);
+        gDetector.setOnDoubleTapListener(this);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        gestureText.setText("onDown");
+        return true;
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        gestureText.setText("onFling");
+        return true;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+        gestureText.setText("onLongPress");
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        gestureText.setText("onScroll");
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+        gestureText.setText("onShowPress");
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        gestureText.setText("onSingleTap");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        gestureText.setText("onDoubleTap");
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        gestureText.setText("onDoubleTapEvent");
+        return true;
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        gestureText.setText("onSingleTapConfirmed");
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_common_gestures, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
